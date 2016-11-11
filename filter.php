77cdb64708951bec9f29f9f71afff11373c0d7cd
valery <valery.fremaux@gmail.com>
2016-11-11 23:24:26 +0100

    Automated sync from SVN

--- filter.php
+++ filter.php
@@ -1,35 +1,52 @@
-<?php // $id$
-//////////////////////////////////////////////////////////////
-//  Multiroot filtering
-//  
-// This filter filters all texts searching for original 
-// wwwroot to translate to the aliased used domain
+<?php
+// This file is part of Moodle - http://moodle.org/
 //
-//////////////////////////////////////////////////////////////
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-class multiroot_filter extends moodle_text_filter{
+/**
+ * @package     filter_multiroot
+ * @category    filter
+ * @author      valery.fremaux <valery.fremaux@gmail.com>
+ * @copyright   2010 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ */
 
-	function filter($text) {
-	    global $CFG, $HOSTS_THEMES;
-	    
-	    if (empty($CFG->multiroot)) return $text;
-	    
-	    if (isset($CFG->originalwwwroot) && $CFG->originalwwwroot != $CFG->wwwroot){
-	    	// if we are in an alias, translate orignal contents to current alias
-	    	$text = preg_replace("#{$CFG->originalwwwroot}#", $CFG->wwwroot, $text);
-	    } else {
-	    	// if we are in the original domain, bring back all aliases to the original domain when displaying.
-	    	// Brings back all Alias1, Alias2, AliasN contents to Original
-	    	// this will not solve all crossover situations, specially for content created on Alias1 and 
-	    	// accessed through Alias2
-			if (!empty($HOSTS_THEMES)){
-			    $hostdomains = implode('|', array_keys($HOSTS_THEMES));
-			}
-	    	$text = preg_replace("#http://{$hostdomains}#", $CFG->originalwwwroot, $text);
-	    }
-	
-		return $text;
-	}
+class multiroot_filter extends moodle_text_filter {
+
+    function filter($text) {
+        global $CFG, $CFG;
+
+        if (empty($CFG->multiroot)) {
+            return $text;
+        }
+
+        if (isset($CFG->originalwwwroot) && $CFG->originalwwwroot != $CFG->wwwroot) {
+            // If we are in an alias, translate orignal contents to current alias.
+            $text = preg_replace("#{$CFG->originalwwwroot}#", $CFG->wwwroot, $text);
+        } else {
+            /*
+             * if we are in the original domain, bring back all aliases to the original domain when displaying.
+             * Brings back all Alias1, Alias2, AliasN contents to Original
+             * this will not solve all crossover situations, specially for content created on Alias1 and 
+             * accessed through Alias2
+             */
+            if (!empty($CFG->hosts_themes)) {
+                $hostdomains = implode('|', array_keys($CFG->hosts_themes));
+            }
+            $text = preg_replace("#http://{$hostdomains}#", $CFG->originalwwwroot, $text);
+        }
+
+        return $text;
+    }
 }
 
-?>
